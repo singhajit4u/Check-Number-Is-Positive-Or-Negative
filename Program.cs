205005ba9193d5341aba58f02990509aa539b7da
singhajit4u <ajit.co.info@gmail.com>
2023-02-24 23:33:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Check_Number_Is_Positive_Or_Negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to test Positive Or Negative:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0) 
+            {
+                Console.WriteLine("Positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+        }
+    }
+}
